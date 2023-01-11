870e236615bbb21b87589c5d6776c68a3717db24
chosokabe1 <tktkshi101110@gmail.com>
2023-01-12 03:12:42 +0900

    aa

--- cv.py
+++ cv.py
@@ -3,6 +3,7 @@ import glob
 import os
 from PIL import Image
 import numpy as np
+import shutil
 
 def folder_gray2BGR(folder):
   imgz_paths = []
@@ -16,6 +17,10 @@ def folder_gray2BGR(folder):
     img = cv2.imread(path)
     cv2.imwrite(path,img)
   
+def copy_folder(from_folder, to_folder):
+  for from_file in glob.glob(os.path.join(from_folder,'*')):
+    shutil.copy(from_file, to_folder)
+
 def makedir(path):
   if not os.path.exists(path):
     os.makedirs(path)
@@ -81,6 +86,29 @@ def expand2square(pil_img, background_color):
         result.paste(pil_img, ((height - width) // 2, 0))
         return result
         
+def padding_0_min_size_img(img, min_size):
+  h, w, _ = img.shape
+  flag = False
+  if h < min_size:
+    newimg = np.zeros((min_size, w, 3))
+    start = int((min_size - h) / 2)
+    fin = int((min_size + h) / 2)
+    newimg[start:fin, :] = img
+    flag = True
+  
+  if w < min_size:
+    newimg = np.zeros((h, min_size, 3))
+    start = int((min_size - w) / 2)
+    fin = int((min_size + w) / 2)
+    newimg[:, start:fin] = img
+    flag = True
+  
+  if flag:
+    return newimg
+  else:
+    return img
+
+
 def make_square_file(in_path, out_path):
   out_img_path = out_path + "\\" + os.path.basename(in_path)
   im = Image.open(in_path)
@@ -94,3 +122,34 @@ def output_overall_length(anaume, skeleton_name="aaa", skeleton_path="skeleton_p
   
   overall_length = cv2.countNonZero(skeleton)
   return overall_length
+
+def folder_bright_kakutyou(in_folder, out_folder):
+  gamma1 = 0.5
+  gamma2 = 0.75
+  gamma3 = 1.25
+  gamma4 = 1.5
+  img2gamma1 = np.zeros((256,1),dtype=np.uint8)
+  img2gamma2 = np.zeros((256,1),dtype=np.uint8)
+  img2gamma3 = np.zeros((256,1),dtype=np.uint8)
+  img2gamma4 = np.zeros((256,1),dtype=np.uint8)
+
+  for i in range(256):
+    img2gamma1[i][0] = 255 * (float(i)/255) ** (1.0 / gamma1)
+    img2gamma2[i][0] = 255 * (float(i)/255) ** (1.0 / gamma2)
+    img2gamma3[i][0] = 255 * (float(i)/255) ** (1.0 / gamma3)
+    img2gamma4[i][0] = 255 * (float(i)/255) ** (1.0 / gamma4)
+
+
+  for idx,in_path in enumerate(glob.glob(os.path.join(in_folder, '*'))):
+    img = cv2.imread(in_path)
+    if idx % 4 == 0:
+      gamma_img = cv2.LUT(img,img2gamma1)
+    elif idx % 4 == 1:
+      gamma_img = cv2.LUT(img,img2gamma2)
+    elif idx % 4 == 2:
+      gamma_img = cv2.LUT(img,img2gamma3)
+    else:
+      gamma_img = cv2.LUT(img,img2gamma4)
+
+    cv2.imwrite(os.path.join(out_folder, os.path.basename(in_path)), img)
+    cv2.imwrite(os.path.join(out_folder, os.path.splitext(os.path.basename(in_path))[0] +'_bright.jpg'), gamma_img)
