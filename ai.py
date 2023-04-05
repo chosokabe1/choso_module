5154acceef94c7ea4b3fbd37b9804a9dda630d0c
chosokabe1 <tktkshi101110@gmail.com>
2023-04-05 23:24:47 +0900

    efficient

--- ai.py
+++ ai.py
@@ -43,108 +43,69 @@ def set_parameter_requires_grad(model, feature_extracting):
   if feature_extracting:
     for param in model.parameters():
       param.requires_grad = False
+
 def makedir(dir):
   if not os.path.exists(dir):
     os.makedirs(dir)
-
+from swin_transformer_pytorch import SwinTransformer
 from efficientnet_pytorch import EfficientNet
 def initialize_model(model_name, num_classes, feature_extract, use_pretrained=True, binary=False):
   # Initialize these variables which will be set in this if statement. Each of these
   #   variables is model specific.
   model_ft = None
   input_size = 0
-  if model_name == "resnet":
-    """ Resnet18
-    """
-    model_ft = models.resnet18(pretrained=use_pretrained)
+  if "resnet" in model_name:
+    if model_name == "resnet18":
+      model_ft = models.resnet18(pretrained=use_pretrained)
+      num_ftrs = model_ft.fc.in_features
+    
+    elif model_name == "resnet34":
+      model_ft = models.resnet34(pretrained=use_pretrained)
+      num_ftrs = model_ft.fc.in_features
+
+    elif model_name == "resnet50":
+      model_ft = models.resnet50(pretrained=use_pretrained)
+      num_ftrs = model_ft.fc.in_features
+
     set_parameter_requires_grad(model_ft, feature_extract)
-    num_ftrs = model_ft.fc.in_features
     if binary:
       model_ft.conv1 = nn.Conv2d(
-        in_channels=1,
-        out_channels=64,
-        kernel_size=model_ft.conv1.kernel_size,
-        stride=model_ft.conv1.stride,
-        padding=model_ft.conv1.padding,
-        bias=False
-      )
+      in_channels=1,
+      out_channels=64,
+      kernel_size=model_ft.conv1.kernel_size,
+      stride=model_ft.conv1.stride,
+      padding=model_ft.conv1.padding,
+      bias=False
+    )
     model_ft.fc = nn.Linear(num_ftrs, num_classes)
     input_size = 224
 
-  elif model_name == "resnet_480":
-    """ Resnet18
-    """
-    model_ft = models.resnet18(pretrained=use_pretrained)
-    set_parameter_requires_grad(model_ft, feature_extract)
-    num_ftrs = model_ft.fc.in_features
+  elif "swin" in model_name:
+    if model_name == "swin_t":
+        model_ft = SwinTransformer(hidden_dim=96,
+                                   layers=(2, 2, 6, 2),
+                                   heads=(3, 6, 12, 24),
+                                   channels=3,
+                                   num_classes=2,
+                                   head_dim=32,
+                                   window_size=7,
+                                   downscaling_factors=(4, 2, 2, 2),
+                                   relative_pos_embedding=True)
+        
+        input_size = 224
+
+    elif model_name == "swin_v2_t":
+        model_ft = SwinTransformer(img_size=224, patch_size=4, in_chans=3,
+                                   num_classes=num_classes, embed_dim=96,
+                                   depths=[2, 2, 6, 2], num_heads=[3, 6, 12, 24],
+                                   window_size=7, mlp_ratio=4, qkv_bias=True,
+                                   use_absolute_pos_emb=True, drop_path_rate=0.1)
+        input_size = 224
+
     if binary:
-      model_ft.conv1 = nn.Conv2d(
-        in_channels=1,
-        out_channels=64,
-        kernel_size=model_ft.conv1.kernel_size,
-        stride=model_ft.conv1.stride,
-        padding=model_ft.conv1.padding,
-        bias=False
-      )
-    model_ft.fc = nn.Linear(num_ftrs, num_classes)
-    input_size = 480
+        model_ft.patch_embed = nn.Conv2d(1, model_ft.embed_dim, kernel_size=model_ft.patch_size, stride=model_ft.patch_size, padding=0)
 
-  elif model_name == "resnet_1800_gray":
-    """ Resnet18 with 1800x1800 grayscale input
-    """
-    if use_pretrained:
-      print("Pretrained weights are not available for this model configuration. Initializing without pretrained weights.")
-    model_ft = models.resnet18(pretrained=False)
     set_parameter_requires_grad(model_ft, feature_extract)
-    num_ftrs = model_ft.fc.in_features
-    model_ft.conv1 = nn.Conv2d(
-      in_channels=1,
-      out_channels=64,
-      kernel_size=model_ft.conv1.kernel_size,
-      stride=model_ft.conv1.stride,
-      padding=model_ft.conv1.padding,
-      bias=False
-    )
-    model_ft.fc = nn.Linear(num_ftrs, num_classes)
-    input_size = 1800
-
-  elif model_name == "resnet_1024_gray":
-    """ Resnet18 with 1024x1024 grayscale input
-    """
-    if use_pretrained:
-      print("Pretrained weights are not available for this model configuration. Initializing without pretrained weights.")
-    model_ft = models.resnet18(pretrained=False)
-    set_parameter_requires_grad(model_ft, feature_extract)
-    num_ftrs = model_ft.fc.in_features
-    model_ft.conv1 = nn.Conv2d(
-      in_channels=1,
-      out_channels=64,
-      kernel_size=model_ft.conv1.kernel_size,
-      stride=model_ft.conv1.stride,
-      padding=model_ft.conv1.padding,
-      bias=False
-    )
-    model_ft.fc = nn.Linear(num_ftrs, num_classes)
-    input_size = 1024
-  
-  elif model_name == "resnet_512_gray":
-    """ Resnet18 with 1024x1024 grayscale input
-    """
-    if use_pretrained:
-      print("Pretrained weights are not available for this model configuration. Initializing without pretrained weights.")
-    model_ft = models.resnet18(pretrained=False)
-    set_parameter_requires_grad(model_ft, feature_extract)
-    num_ftrs = model_ft.fc.in_features
-    model_ft.conv1 = nn.Conv2d(
-      in_channels=1,
-      out_channels=64,
-      kernel_size=model_ft.conv1.kernel_size,
-      stride=model_ft.conv1.stride,
-      padding=model_ft.conv1.padding,
-      bias=False
-    )
-    model_ft.fc = nn.Linear(num_ftrs, num_classes)
-    input_size = 512
     
   elif model_name == "vit_l_16":
     model_ft = models.vit_b_16(weights=models.ViT_B_16_Weights.DEFAULT)
@@ -162,14 +123,63 @@ def initialize_model(model_name, num_classes, feature_extract, use_pretrained=Tr
     model_ft.fc = nn.Linear(num_ftrs, num_classes)
     input_size = 480
 
-     
+  if "efficientnet-b" in model_name:
+    first_layer_out_channels_map = {
+        "efficientnet-b0": 32,
+        "efficientnet-b1": 32,
+        "efficientnet-b2": 32,
+        "efficientnet-b3": 40,
+        "efficientnet-b4": 48,
+        "efficientnet-b5": 48,
+        "efficientnet-b6": 56,
+        "efficientnet-b7": 64,
+    }
+    if model_name == "efficientnet-b0":
+      model_ft = EfficientNet.from_pretrained('efficientnet-b0')
+      num_ftrs = model_ft._fc.in_features
+      input_size = 224
 
-  elif model_name == "efficientnetb7":
-    model_ft = EfficientNet.from_pretrained('efficientnet-b7')
+    elif model_name == "efficientnet-b1":
+      model_ft = EfficientNet.from_pretrained('efficientnet-b1')
+      num_ftrs = model_ft._fc.in_features
+      input_size = 240
+
+    elif model_name == "efficientnet-b2":
+      model_ft = EfficientNet.from_pretrained('efficientnet-b2')
+      num_ftrs = model_ft._fc.in_features
+      input_size = 260
+  
+    elif model_name == "efficientnet-b3":
+      model_ft = EfficientNet.from_pretrained('efficientnet-b3')
+      num_ftrs = model_ft._fc.in_features
+      input_size = 300
+  
+    elif model_name == "efficientnet-b4":
+      model_ft = EfficientNet.from_pretrained('efficientnet-b4')
+      num_ftrs = model_ft._fc.in_features
+      input_size = 380
+
+    elif model_name == "efficientnet-b5":
+      model_ft = EfficientNet.from_pretrained('efficientnet-b5')
+      num_ftrs = model_ft._fc.in_features
+      input_size = 456
+
+    elif model_name == "efficientnet-b6":
+      model_ft = EfficientNet.from_pretrained('efficientnet-b6')
+      num_ftrs = model_ft._fc.in_features
+      input_size = 528
+
+    elif model_name == "efficientnet-b7":
+      model_ft = EfficientNet.from_pretrained('efficientnet-b7')
+      num_ftrs = model_ft._fc.in_features
+      input_size = 600
+
+    if binary:
+      out_channels = first_layer_out_channels_map[model_name]
+      model_ft._conv_stem = nn.Conv2d(1, out_channels, kernel_size=3, stride=2, bias=False)
+
     set_parameter_requires_grad(model_ft, feature_extract)
-    num_ftrs = model_ft._fc.in_features
-    model_ft._fc = nn.Linear(num_ftrs, num_classes)
-    input_size = 600
+    model_ft.fc = nn.Linear(num_ftrs, num_classes)
 
   elif model_name == "efficientnetv2m":
     model_ft = EfficientNet.from_pretrained('efficientnet-b7')
@@ -404,6 +414,8 @@ def tensor_to_np(inp, type):
 
 def false_img_save(pred, label, input, false_img_count, out_dir, class_names):
   pil_img = Image.fromarray(input)
+  print(pred,label,class_names)
+
   makedir(out_dir + 'error/pred_' + str(class_names[pred.item()]) + '_label_' + str(class_names[label.item()]))
   pil_img.save(out_dir + f'error/pred_{class_names[pred.item()]}_label_{class_names[label.item()]}/{false_img_count}.jpg')
 
